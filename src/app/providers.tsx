af1e3d855a299b8925bb714f282d02be02603918
Samin Islam <saminsss@gmail.com>
2023-11-04 23:21:27 -0700

    setup chakra ui

---
+++ src/app/providers.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { ChakraProvider } from '@chakra-ui/react'
+
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+  primary: '#3898ec',
+  secondary: '#fff',
+  tertiary: '#8bc220',
+}
+
+export const theme = extendTheme({ colors })
+
+export function Providers({
+    children 
+  }: { 
+  children: React.ReactNode 
+  }) {
+  return (
+    <ChakraProvider theme={theme}>
+      {children}
+    </ChakraProvider>
+  )
+}
