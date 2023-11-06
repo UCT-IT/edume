fd02aa79122e71227e2e1a019bf22e4089f4e93d
Md. Jobaye Hossain <jobayerh.uctitbd@gmail.com>
2023-11-06 15:04:31 +0600

    communication section added in feature

--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import Feature from './Feature/Feature';
-import PaidSection from './PaidSection/PaidSection';
-import Clients from './Clients/Clients';
+import React from "react";
+import Feature from "./Feature/Feature";
+import PaidSection from "./PaidSection/PaidSection";
+import Clients from "./Clients/Clients";
+import Communication from "./Communication/Communication";
 
 const Features = () => {
-    return (
-        <>
-          <Feature /> 
-          <PaidSection />
-          <Clients />
-        </>
-    );
+  return (
+    <>
+      <Feature />
+      <PaidSection />
+      <Clients />
+      <Communication />
+    </>
+  );
 };
 
 export default Features;
