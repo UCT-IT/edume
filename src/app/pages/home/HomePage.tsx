884dcb6b755784365a426213f6746c8fd3aef595
Md. Jobaye Hossain <jobayerh.uctitbd@gmail.com>
2023-11-21 13:19:55 +0600

    changed in review section

--- src/app/pages/home/HomePage.tsx
+++ src/app/pages/home/HomePage.tsx
@@ -8,7 +8,7 @@ const HomePage = () => {
     <React.Fragment>
       <HeroSection />
       <Features />
-      <TestimonialCarousel></TestimonialCarousel>
+      <TestimonialCarousel />
     </React.Fragment>
   );
 };
