72c5f21bd8d30051882fc8bed0d11310c4e25251
Rasheduluctit <rasheduli.uctitbd@gmail.com>
2023-11-29 17:25:07 +0600

    Chakra Ui Uninstalled

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,28 +1,22 @@
-"use client";
+// const colors = {
+//   primary: "#3898ec",
+//   secondary: "#fff",
+//   tertiary: "#8bc220",
+//   buttonHoverd: "#5AA8EC",
+//   textColor: "#555",
+//   footerLinksColor: "#ffffffad",
+// };
+// const breakpoints = {
+//   base: "0px",
+//   sm: "576px",
+//   md: "768px",
+//   lg: "992px",
+//   xl: "1280px",
+//   "2xl": "1536px",
+// };
 
-import { ChakraProvider } from "@chakra-ui/react";
+// export const theme = extendTheme({ colors, breakpoints });
 
-import { extendTheme } from "@chakra-ui/react";
-
-const colors = {
-  primary: "#3898ec",
-  secondary: "#fff",
-  tertiary: "#8bc220",
-  buttonHoverd: "#5AA8EC",
-  textColor: "#555",
-  footerLinksColor: "#ffffffad",
-};
-const breakpoints = {
-  base: "0px",
-  sm: "576px",
-  md: "768px",
-  lg: "992px",
-  xl: "1280px",
-  "2xl": "1536px",
-};
-
-export const theme = extendTheme({ colors, breakpoints });
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
-}
+// export function Providers({ children }: { children: React.ReactNode }) {
+//   return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+// }
